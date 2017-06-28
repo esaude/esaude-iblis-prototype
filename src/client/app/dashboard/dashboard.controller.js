f6136dc835fd16c1f89447cd21849c8b7492941d
Yassin Nurmahomed <yassin.nurmahomed@ucglobalprograms.org>
2017-06-28 16:51:00 +0200

    Move charts to dashboard.

--- src/client/app/dashboard/dashboard.controller.js
+++ src/client/app/dashboard/dashboard.controller.js
@@ -17,6 +17,20 @@
     vm.people = [];
     vm.title = 'Dashboard';
 
+    vm.options = {legend: {display: true}};
+
+    vm.labels1 = ["Specimen Accepted", "Specimen Not collected", "Specimen Rejected"];
+    vm.data1 = [300, 500, 100];
+
+    vm.labels2 = ["Started", "Completed", "Verified", "Pending"];
+    vm.data2 = [300, 500, 100, 100];
+
+    vm.labels3 = ["Specimen Accepted", "Specimen Not collected", "Specimen Rejected"];
+    vm.data3 = [257, 450, 95, 88];
+
+    vm.labels4 = ["Started", "Completed", "Verified", "Pending"];
+    vm.data4 = [156, 420, 96, 45];
+
     activate();
 
     function activate() {
